5863c1bbe42ba24c0bb90275fa052a23ce634cf7
dngiang <dongocgiang.edu@gmail.com>
2024-04-17 22:51:37 +0700

    ✨ Update email regex

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
 const validateEmail = (email) => emailRegex.test(email);
 
